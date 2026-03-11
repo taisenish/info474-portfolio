17fd95b97cf24c57c0ecfc15c18cc85cfd9002c3
Taise Nish <taisen@uw.edu>
2026-03-11 01:42:58 -0700

    Add initial bar chart clock sketch
    Add initial horizontal bar chart clock concept for sketch 1

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,14 +1,48 @@
-// Instance-mode sketch for tab 2
-registerSketch('sk2', function (p) {
+registerSketch('sk2', function(p) {
+
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(600, 600);
   };
-  p.draw = function () {
-    p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+  
+  var data = [{
+    type: 'bar',
+    x: [15, 30, 45, 60],
+    y: [1, 2, 3, 4],
+    orientation: 'h',
+  
+    marker: {
+      color: [
+        'rgba(100, 180, 200, 0.5)',
+        'rgba(200, 180, 100, 0.5)',
+        'rgba(100, 200, 180, 0.5)',
+        'rgba(0, 0, 0, 1)'
+      ]
+    }
+  }];
+  
+  var layout = {
+    title: {
+      text: "Hours and Minutes Stacked Chart",
+      font: { size: 24 }
+    },
+  
+    xaxis: {
+      title: {
+        text: "minutes",
+        font: { size: 18 }
+      }
+    },
+  
+    yaxis: {
+      title: {
+        text: "hours",
+        font: { size: 18 }
+      }
+    }
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
-});
+  
+  p.draw = function() {
+    Plotly.newPlot('myDiv', data, layout);
+  };
+  
+  });
