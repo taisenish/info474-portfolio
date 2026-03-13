d4adc4704e4a8d98ad93463663f2a6e9d0d7547e
Taise Nish <taisen@uw.edu>
2026-03-13 02:19:57 -0700

    Update sketch3.js
    Add live seconds to ring clock label

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -10,21 +10,42 @@ registerSketch('sk3', function(p) {
     const x = p.width / 2;
     const y = p.height / 2;
 
-    // Draw the blue progress arc
-    p.noStroke();
-    p.fill(0, 120, 255); // blue
-    p.arc(x, y, 200, 200, -p.HALF_PI, -p.HALF_PI + p.PI / 3, p.PIE);
+    let hr = p.hour();
+    let min = p.minute();
+    let sec = p.second();
 
-    // Cut out the center to make the ring hollow
-    p.fill(220); // same as background
+    let angle = p.map(sec, 0, 60, -p.HALF_PI, p.TWO_PI - p.HALF_PI);
+
+    // outer progress ring
+    p.noStroke();
+    p.fill(0, 120, 255);
+    p.arc(x, y, 200, 200, -p.HALF_PI, angle, p.PIE);
+
+    // hollow center
+    p.fill(220);
     p.circle(x, y, 150);
 
-    // Draw the outlines of the ring
+    // ring outlines
     p.noFill();
     p.stroke(0);
     p.strokeWeight(2);
     p.circle(x, y, 200);
     p.circle(x, y, 150);
+
+    // time label with seconds
+    p.noStroke();
+    p.fill(0);
+    p.textAlign(p.CENTER, p.CENTER);
+
+    p.textSize(24);
+    p.text(
+      p.nf(hr, 2) + ":" + p.nf(min, 2) + ":" + p.nf(sec, 2),
+      x,
+      y - 10
+    );
+
+    p.textSize(16);
+    p.text("seconds", x, y + 22);
   };
 
 });
