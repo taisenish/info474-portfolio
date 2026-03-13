d9c921ec8351143a016b9a727fa0567b6c554dc2
Taise Nish <taisen@uw.edu>
2026-03-13 01:29:58 -0700

    Added sketches and corrected sketch4.js
    Added all drawn sketches of each selected sketch (3) , also fixed the issue of the horizontal bar chart sketch not showing up

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,58 +1,50 @@
-const sketch4 = (p) => {
+registerSketch('sk4', function(p) {
+
   p.setup = function () {
-    p.createCanvas(800, 800);
-    p.angleMode(p.DEGREES);
+    p.createCanvas(600, 600);
   };
-
-  p.draw = function () {
-    p.background(245, 240, 235);
-
-    let cx = p.width / 2;
-    let cy = p.height / 2 - 80;
-
-    drawBaseFlower(cx, cy);
-  };
-
-  function drawBaseFlower(x, y) {
-    // stem
-    p.stroke(70, 140, 90);
-    p.strokeWeight(8);
-    p.line(x, y + 40, x, y + 220);
-
-    // leaves
-    p.noStroke();
-    p.fill(90, 170, 110);
-    p.ellipse(x - 35, y + 140, 50, 25);
-    p.ellipse(x + 35, y + 175, 50, 25);
-
-    // petals
-    p.push();
-    p.translate(x, y);
-
-    for (let i = 0; i < 4; i++) {
-      p.rotate(90);
-      p.fill(160, 190, 235);
-      p.stroke(120, 150, 210);
-      p.strokeWeight(2);
-      drawPetal();
+  
+  var data = [{
+    type: 'bar',
+    x: [15, 30, 45, 60],
+    y: [1, 2, 3, 4],
+    orientation: 'h',
+  
+    marker: {
+      color: [
+        'rgba(100, 180, 200, 0.5)',
+        'rgba(200, 180, 100, 0.5)',
+        'rgba(100, 200, 180, 0.5)',
+        'rgba(0, 0, 0, 1)'
+      ]
     }
+  }];
+  
+  var layout = {
+    title: {
+      text: "Hours and Minutes Stacked Chart",
+      font: { size: 24 }
+    },
+  
+    xaxis: {
+      title: {
+        text: "minutes",
+        font: { size: 18 }
+      }
+    },
+  
+    yaxis: {
+      title: {
+        text: "hours",
+        font: { size: 18 }
+      }
+    }
+  };
+  
+  p.draw = function() {
+    Plotly.newPlot('myDiv4', data, layout);
+  };
+  
+  });
 
-    p.pop();
-
-    // center
-    p.fill(245, 210, 60);
-    p.stroke(200, 170, 40);
-    p.strokeWeight(2);
-    p.circle(x, y, 50);
-  }
-
-  function drawPetal() {
-    p.beginShape();
-    p.vertex(0, 0);
-    p.bezierVertex(-18, -25, -20, -80, 0, -105);
-    p.bezierVertex(20, -80, 18, -25, 0, 0);
-    p.endShape(p.CLOSE);
-  }
-};
-
-new p5(sketch4, "sketch-container-sk4");
+  
